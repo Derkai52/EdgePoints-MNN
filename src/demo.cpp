5c157305744bc34ced39f9b47fbeb81092c6104d
Derkai <2363104914@qq.com>
2025-04-22 17:02:25 +0800

    [model]: 新增模型导出和模型文件.

---
+++ src/demo.cpp
@@ -0,0 +1,51 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include "edgepoints.h"
+
+class EdgePointsNode {
+public:
+    EdgePointsNode(ros::NodeHandle& nh)
+        : it_(nh)
+    {
+        sub1_ = it_.subscribe("/camera/infra1/image_rect_raw", 1, &EdgePointsNode::imageCallback1, this);
+        sub2_ = it_.subscribe("/camera/infra2/image_rect_raw", 1, &EdgePointsNode::imageCallback2, this);
+
+        pub1_ = it_.advertise("/edgepoints/image1", 1);
+        pub2_ = it_.advertise("/edgepoints/image2", 1);
+    }
+
+    void imageCallback1(const sensor_msgs::ImageConstPtr& msg) {
+        try {
+            cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, "mono8");
+            pub1_.publish(cv_ptr->toImageMsg());
+        } catch (cv_bridge::Exception& e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+        }
+    }
+
+    void imageCallback2(const sensor_msgs::ImageConstPtr& msg) {
+        try {
+            cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, "mono8");
+            pub2_.publish(cv_ptr->toImageMsg());
+        } catch (cv_bridge::Exception& e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+        }
+    }
+
+private:
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber sub1_, sub2_;
+    image_transport::Publisher pub1_, pub2_;
+};
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "edgepoints");
+    ros::NodeHandle nh;
+
+    EdgePointsNode node(nh);
+    ros::spin();
+    return 0;
+}
